cdcbdc0f851bf45ff724e338cd8c9995abb34532
no-u <81192508+no-u101@users.noreply.github.com>
2022-04-08 20:06:14 -0700

    Update core.js

--- js/core.js
+++ js/core.js
@@ -136,22 +136,12 @@ window.onload = async () => {
     const PC = window.noodle.releases.PC = pc_data.filter(release => release["tag_name"].includes("noodle"))[0];
     console.log("Loaded PC");
     
-    // BeatMods
-    const bm_data = await (await fetch("https://beatmods.com/api/v1/mod")).json();
-    for(let i = 0; i < bm_data.length; i++){ 
-        if (i.name == "NoodleExtensions" && i.version == PC.tag_name.replace("noodleextensions-v", "") && i.status == "approved") {
-            basebmurl = "https://beatmods.com"
-            filelocation = i.downloads[0].url
-            bmdownloadurl = (basebmurl + filelocation)
-            break;
-        }
-    } 
     
     $("#pc_version").html(PC.tag_name.replace("noodleextensions-v", ""));
     $("#pc_download_btn").attr("href", PC.assets[0].browser_download_url);
     $("#pc_title").html(PC.name);
     $("#pc_link").attr("href", PC.html_url);
-    $("#beatmods_link").attr("href", bmdownloadurl);
+    $("#beatmods_link").attr("href", "/");
 
     $("#pcLoadingDoneToRemove").css("display", "initial");
     $("#pcLoadingIndicator").remove();
